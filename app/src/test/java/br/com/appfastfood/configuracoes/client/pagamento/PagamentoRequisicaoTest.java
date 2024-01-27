08b344f51ae8fca378b7aaaec3f65637f39bff17
caikegburgos <caikegabriel1997@gmail.com>
2024-01-27 12:28:51 -0300

    Testes unitários pagamento

---
+++ app/src/test/java/br/com/appfastfood/configuracoes/client/pagamento/PagamentoRequisicaoTest.java
@@ -0,0 +1,29 @@
+package br.com.appfastfood.configuracoes.client.pagamento;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PagamentoRequisicaoTest {
+
+    @Test
+    void gettersAndSetters_CorretamenteImplementados() {
+        // Arrange
+        String meioPagamento = "Cartao";
+        String idMeioPagamento = "1234567891011121";
+        Double valor = 100.00;
+
+        // Act
+        PagamentoRequisicao requisicao = new PagamentoRequisicao(meioPagamento, idMeioPagamento, valor);
+        requisicao.setMeioPagamento(meioPagamento);
+        requisicao.setIdMeioPagamento(idMeioPagamento);
+        requisicao.setValor(valor);
+
+        // Assert
+        assertEquals(meioPagamento, requisicao.getMeioPagamento());
+        assertEquals(idMeioPagamento, requisicao.getIdMeioPagamento());
+        assertEquals(valor, requisicao.getValor());
+    }
+}
