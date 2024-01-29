1c19ee75b6ede446835dfaf45f95e48472773166
mvgv <mvgv1989@gmail.com>
2024-01-28 22:18:25 -0300

    chore: esteira para deployar no eks

--- app/src/main/java/br/com/appfastfood/configuracoes/client/carrinho/CarrinhoClient.java
+++ app/src/main/java/br/com/appfastfood/configuracoes/client/carrinho/CarrinhoClient.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(url = "http://a63ca3d0b51b849349ddff3fbc1f438a-f36c87df02ec6970.elb.us-east-1.amazonaws.com/", name = "Carrinho")
+@FeignClient(name = "appfastfood-producao")
 public interface CarrinhoClient {
 
-    @GetMapping("carrinho")
+    @GetMapping("/carrinho")
     List<Carrinho> getCarrinho();
 
-    @DeleteMapping("carrinho/{id}")
+    @DeleteMapping("/carrinho/{id}")
     void deleteCarrinho(@RequestParam Long id);
 }
