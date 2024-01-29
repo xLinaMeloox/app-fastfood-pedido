92fc8ec4dcf35519801935b5e8e8d4b4e18864f6
mvgv <mvgv1989@gmail.com>
2024-01-28 22:22:47 -0300

    chore: esteira para deployar no eks

--- app/src/main/java/br/com/appfastfood/AppFastfoodApplication.java
+++ app/src/main/java/br/com/appfastfood/AppFastfoodApplication.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
+@EnableDiscoveryClient
 @OpenAPIDefinition(info = @Info(
 		title = "API - Fast Food",
 		version = "0.0.3-SNAPSHOT",
